7761ca10eaeda4640bbbe149917ac396462e31e5
Walczakp007 <walczakp007@gmail.com>
2017-02-26 16:28:14 +0100

    Caching idea by @nastra

--- WordNet/src/Outcast.java
+++ WordNet/src/Outcast.java
@@ -2,4 +2,29 @@
  * Created by Paul on 2017-02-26.
  */
 public class Outcast {
+    private final WordNet wordnet;
+
+    public Outcast(WordNet wordnet){
+        this.wordnet = wordnet;
+    }         // constructor takes a WordNet object
+
+    public String outcast(String[] nouns) {
+        int maxDistance = 0;
+        String outcast = "";
+        for(String nounA: nouns) {
+            int distance = 0;
+            for (String nounB : nouns)
+                if (nounA != nounB) {
+                    distance += wordnet.distance(nounA, nounB);
+                }
+            if(distance > maxDistance){
+                maxDistance = distance;
+                outcast = nounA;
+            }
+        }
+        return outcast;
+    }  // given an array of WordNet nouns, return an outcast
+    public static void main(String[] args){
+
+    }  // see test client below
 }
--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -1,17 +1,50 @@
 import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Paul on 2017-02-26.
  */
+
+/**
+ * Ingenious idea of caching , by @nastra
+ */
 public class SAP {
     private Digraph digraph;
+    private Map<String, FastSAP> cache;
+
 
     public SAP(Digraph digraph){
         this.digraph = digraph;
+        this.cache = new HashMap<>();
+    }
+
+    private FastSAP cachedResult(int v, int w) {
+        String key = v + "_" + w;
+        if (cache.containsKey(key)) {
+            FastSAP p = cache.get(key);
+            // we need to cache only for 2 consecutive calls, therefore we delete the result after the second call
+            cache.remove(key);
+            return p;
+        }
+        FastSAP p = new FastSAP(v, w);
+        cache.put(key, p);
+        return p;
+    }
+
+    private FastSAP cachedResult(Iterable<Integer> v, Iterable<Integer> w) {
+        String key = v.toString() + "_" + w.toString();
+        if (cache.containsKey(key)) {
+            FastSAP p = cache.get(key);
+            // we need to cache only for 2 consecutive calls, therefore we delete the result after the second call
+            cache.remove(key);
+            return p;
+        }
+
+        FastSAP p = new FastSAP(v, w);
+        cache.put(key, p);
+        return p;
     }
 
     private boolean validIndex(int index) {
@@ -28,79 +61,79 @@ public class SAP {
         return true;
     }
 
-    // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w){
-        if(!validIndex(v) || !validIndex(w))
-            throw new IndexOutOfBoundsException();
-        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
-        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
-        int commonAncestor = ancestor(v,w);
-        if(commonAncestor == - 1)
-            return -1;
-        else
-            return bfsV.distTo(commonAncestor) + bfsW.distTo(commonAncestor);
+        if (!validIndex(v) || !validIndex(w)) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return cachedResult(v,w).distance;
     }
 
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w){
-        if(!validIndex(v) || !validIndex(w))
-            throw new IndexOutOfBoundsException();
-        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
-        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
-
-        //in case v and w are the same - checklist
-        if(v == w)
-            return v;
-        int minDistance = Integer.MAX_VALUE;
-        int commonShortestAncestor = -1;
-        for(int i = 0; i < digraph.V(); i++)
-            if(bfsV.hasPathTo(i) && bfsW.hasPathTo(w)) {
-                int tempDistance = bfsV.distTo(i) + bfsW.distTo(i);
-                if (tempDistance < minDistance) {
-                    minDistance = tempDistance;
-                    commonShortestAncestor = i;
-                }
-
-            }
-        return commonShortestAncestor;
-    }
-
-    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w){
-        if(!validIndex(v) || !validIndex(w))
-            throw new IndexOutOfBoundsException();
-        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
-        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
-        int commonAncestor = ancestor(v,w);
-        if(commonAncestor == - 1)
-            return -1;
-        else
-            return bfsV.distTo(commonAncestor) + bfsW.distTo(commonAncestor);
+        if (!validIndex(v) || !validIndex(w)) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return cachedResult(v,w).distance;
     }
 
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
-        if(!validIndex(v) || !validIndex(w))
-            throw new IndexOutOfBoundsException();
-        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
-        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
+    public int ancestor(int v, int w) {
+        if (!validIndex(v) || !validIndex(w)) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return cachedResult(v, w).ancestor;
+    }
 
-        //in case v and w are the same - checklist
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        if (!validIndex(v) || !validIndex(w)) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return cachedResult(v, w).ancestor;
+    }
 
-        int minDistance = Integer.MAX_VALUE;
-        int commonShortestAncestor = -1;
-        for(int i = 0; i < digraph.V(); i++)
-            if(bfsV.hasPathTo(i) && bfsW.hasPathTo(i)) {
-                int tempDistance = bfsV.distTo(i) + bfsW.distTo(i);
-                if (tempDistance < minDistance) {
-                    minDistance = tempDistance;
-                    commonShortestAncestor = i;
+
+    private class FastSAP{
+        private int distance;
+        private int ancestor;
+
+        public FastSAP(int v, int w){
+            BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
+            BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
+            calculate(bfsV, bfsW);
+        }
+
+        public FastSAP(Iterable<Integer> v, Iterable<Integer> w){
+            BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(digraph, v);
+            BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(digraph, w);
+            calculate(bfsV, bfsW);
+        }
+
+        private void calculate(BreadthFirstDirectedPaths bfsV, BreadthFirstDirectedPaths bfsW){
+            //String concatenatedKey = v + "." + w;
+            List<Integer> ancestors = new ArrayList<>();
+            for (int i = 0; i < digraph.V(); i++) {
+                if (bfsV.hasPathTo(i) && bfsW.hasPathTo(i)) {
+                    ancestors.add(i);
                 }
+            }
+
+            int shortestAncestor = -1;
+            int minDistance = Integer.MAX_VALUE;
+            for (int ancestor : ancestors) {
+                int dist = bfsV.distTo(ancestor) + bfsW.distTo(ancestor);
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    shortestAncestor = ancestor;
+                }
+            }
+            if (Integer.MAX_VALUE == minDistance) {
+                distance = -1;
+            } else {
+                distance = minDistance;
 
             }
-        return commonShortestAncestor;
-    }
+            ancestor = shortestAncestor;
+        }
 
+    }
     // do unit testing of this class
     public static void main(String[] args){
 
